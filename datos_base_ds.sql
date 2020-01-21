7ff812eb499e74d116a42991819dd38557cf5313
Luis Anchundia <leanchun@espol.edu.ec>
2020-01-21 09:03:38 -0500

    datos

---
+++ datos_base_ds.sql
@@ -0,0 +1,15 @@
+use myhome;
+insert into persona values("Odalys", "Guzman", "0912546987","0321457896","oyeguzman@hotmail.com","Alborada","042569878","Casada",21,"jefe de sistemas",1);
+insert into persona values("Alejandro", "Mora", "0926578977","0965784215","alejandrorojo@hotmail.com","Sur por donde no entra el diablo","042547964","Soltero",21,"Recepcionista",1);
+insert into persona values("Georgina", "Manzano", "120564789","0923465241","georgi@hotmail.com","Kennedy nueva","042456213","Casada",21,"Productora general",1);
+insert into persona values("Luis", "Anchundia", "0953394566","0967035384","luis@hotmail.com","Mapasingue","042033699","Casado",21,"Gerente",1);
+
+
+
+insert into empleado values("Administrador","0912546987");
+insert into empleado values("Vendedor","0926578977");
+
+insert into cliente values("Citty office", "Claro",1,"0953394566");
+insert into casa values(63,1,0,"Sur",0,2,2,"Nacional","Italiana","Tradicional",1,0,"0953394566");
+
+insert into registro values("oyguzman","12345","0912546987","0953394566");
